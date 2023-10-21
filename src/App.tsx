a6f26a5fb270fd49bb4e301d0c3340d5c5984c11
Artem Chebotarev <AChebotarev@niisokb.ru>
2023-10-21 19:07:04 -0400

    feat: VideoGridItem component is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import { useState } from 'react';
 import './App.css';
 import { CategoryPills } from './components/CategoryPills';
-import { categories } from './data/home';
+import { categories, videos } from './data/home';
 import { PageHeader } from './layouts/PageHeader';
+import { VideoGridItem } from './components/VideoGridItem';
 
 export const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   return (
     <div className='max-h-screen flex flex-col'>
+      {/* Header */}
       <PageHeader />
+
       <div className='grid grid-cols-[auto,1fr] flex-grow overflow-auto'>
         <div>Sidebar</div>
         <div className='overflow-x-hidden px-8 pb-4'>
-          <div className='sticky top-0 bg-white z-10 pd-4'>
+          {/* Category Section */}
+          <div className='sticky top-0 bg-white z-10 pb-4'>
             <CategoryPills
               categories={categories}
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
           </div>
-          <div>Main Content</div>
+          <div className='grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]'>
+            {videos.map((el) => (
+              <VideoGridItem key={el.id} {...el} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
